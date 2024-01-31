c3084d7584b195e293859117d5eca5807e520d6c
ArwinMichaelFernandes <arvindfernandes00@gmail.com>
2024-01-31 09:27:43 +0530

    commit1

---
+++ src/casetoolexam/caterpillar_xxx.java
@@ -0,0 +1,18 @@
+package casetoolexam;
+
+public class caterpillar_xxx {
+    
+	int move,cabbage;
+	
+	caterpillar_xxx(int move,int cabbage){
+		this.move=move;
+		this.cabbage=cabbage;
+	}
+	
+	public static void main(String[] args) {
+		
+		caterpillar_xxx c = new caterpillar_xxx(9,7);
+		
+	}
+	
+}
